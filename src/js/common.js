5c6afc40be5b272405d3476d0fdc9d4df7a7acb4
efri-yang <948061564@qq.com>
2018-03-14 10:02:54 +0800

    c

---
+++ src/js/common.js
@@ -0,0 +1,7 @@
+
+layui.config({
+  base: '/js/layuiextend/' //假设这是你存放拓展模块的根目录
+}).extend({ //设定模块别名
+  sideMenu: 'sidemenu',
+  throttleDebounce:"jquery.ba-throttle-debounce"
+});
--- src/layui/layui.js
+++ src/layui/layui.js
@@ -547,12 +547,6 @@
 
 
 
-layui.config({
-  base: '/layui/extend/' //假设这是你存放拓展模块的根目录
-}).extend({ //设定模块别名
-  sideMenu: 'sidemenu',
-  throttleDebounce:"jquery.ba-throttle-debounce"
-});
 
 
 // layui.extend({
